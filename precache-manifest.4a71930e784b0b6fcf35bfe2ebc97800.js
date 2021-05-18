d0e57b43f2bdb391995b39e7f66d173500f28111
EnvyMON <55094587+EnvyMON@users.noreply.github.com>
2021-05-18 23:48:33 +0900

    Add files via upload

---
+++ precache-manifest.4a71930e784b0b6fcf35bfe2ebc97800.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1dd64ce4da44d5478a",
+    "url": "/bcvuestar/css/app.634f5ead.css"
+  },
+  {
+    "revision": "d34f35d282ed7712f0dc67f7d7476417",
+    "url": "/bcvuestar/follower.json"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/bcvuestar/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d25ec163dd94241d6f04fcac6f6869b6",
+    "url": "/bcvuestar/index.html"
+  },
+  {
+    "revision": "ae1dd64ce4da44d5478a",
+    "url": "/bcvuestar/js/app.3c69e05e.js"
+  },
+  {
+    "revision": "96ede6f65361af479167",
+    "url": "/bcvuestar/js/chunk-vendors.6cb5d206.js"
+  },
+  {
+    "revision": "6a1ad00dd7d473f746f3897d0ce8c52e",
+    "url": "/bcvuestar/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bcvuestar/robots.txt"
+  }
+]);
